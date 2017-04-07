24c592d7d279fabe5c52d1e25c1214e72f917369
mucktar <git@github.com:mucktarVIP/fcm_manager.git>
2017-04-07 09:57:34 +0700

    add dev redis configuration

--- app/config/app.js
+++ app/config/app.js
@@ -5,8 +5,6 @@ Confidence = require('confidence');
 store = new Confidence.Store({
   server: {
     $filter: 'env',
-    dev: {},
-    production: {},
     $default: {
       host: '127.0.0.1',
       port: 8000
@@ -14,7 +12,19 @@ store = new Confidence.Store({
   },
   redis: {
     $filter: 'env',
-    dev: {},
+    dev: {
+      connection: {
+        host: 'vip-web-session-stg.czrxsu.0001.apse1.cache.amazonaws.com',
+        port: 6379
+      },
+      db: {
+        userFcm: 1,
+        cartAbandonment: 0,
+        bankTransferExpiration: 3,
+        pointExpiration: 4,
+        shipment: 5
+      }
+    },
     production: {},
     $default: {
       connection: {
@@ -32,16 +42,12 @@ store = new Confidence.Store({
   },
   fcm: {
     $filter: 'env',
-    dev: {},
-    production: {},
     $default: {
       serverKey: 'AAAA9yvVNzQ:APA91bFplvprQicC1rzYKMv9O9OzFS3nKI8DUiH1Ivkfv6jIg7z4gDWKjN92gRWKJZ5uDob4r4aPE36uwaTmnqwShHQ4bo8UR5yI0T0j6fOmtnQH_ZSCTV-v9rH0tdyCYmOfgjrfXia8-YG-APrPsYTz30kB1Od4Rw'
     }
   },
   lifetimes: {
     $filter: 'env',
-    dev:[],
-    production: [],
     $default: [
       1000 * 60 * 60 * 3,
       1000 * 60 * 60 * 24 * 3,
