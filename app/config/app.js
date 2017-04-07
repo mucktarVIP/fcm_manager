1c4dd22d76c544f4b7322438c5f09ac96c67d325
mucktar <git@github.com:mucktarVIP/fcm_manager.git>
2017-04-07 10:11:58 +0700

    dev environtment

--- app/config/app.js
+++ app/config/app.js
@@ -5,6 +5,7 @@ Confidence = require('confidence');
 store = new Confidence.Store({
   server: {
     $filter: 'env',
+    production: {},
     $default: {
       host: '127.0.0.1',
       port: 8000
@@ -42,12 +43,14 @@ store = new Confidence.Store({
   },
   fcm: {
     $filter: 'env',
+    production: {},
     $default: {
       serverKey: 'AAAA9yvVNzQ:APA91bFplvprQicC1rzYKMv9O9OzFS3nKI8DUiH1Ivkfv6jIg7z4gDWKjN92gRWKJZ5uDob4r4aPE36uwaTmnqwShHQ4bo8UR5yI0T0j6fOmtnQH_ZSCTV-v9rH0tdyCYmOfgjrfXia8-YG-APrPsYTz30kB1Od4Rw'
     }
   },
   lifetimes: {
     $filter: 'env',
+    production: {},
     $default: [
       1000 * 60 * 60 * 3,
       1000 * 60 * 60 * 24 * 3,
