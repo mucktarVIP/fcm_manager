9d395b2fed2d43f6b44fb8b2d31b58d3027f0880
mucktar <git@github.com:mucktarVIP/fcm_manager.git>
2017-04-07 14:46:07 +0700

    add production config

--- app/config/app.js
+++ app/config/app.js
@@ -13,7 +13,10 @@ store = new Confidence.Store({
       host: '0.0.0.0',
       port: 80
     },
-    production: {}
+    production: {
+      host: '0.0.0.0',
+      port: 80
+    }
   },
   redis: {
     $filter: 'env',
@@ -43,7 +46,19 @@ store = new Confidence.Store({
         shipment: 5
       }
     },
-    production: {}
+    production: {
+      connection: {
+        host: 'vip-web-session-stg.czrxsu.0001.apse1.cache.amazonaws.com',
+        port: 6379
+      },
+      db: {
+        userFcm: 1,
+        cartAbandonment: 2,
+        bankTransferExpiration: 3,
+        pointExpiration: 4,
+        shipment: 5
+      }
+    }
   },
   fcm: {
     serverKey: 'AAAA9yvVNzQ:APA91bFplvprQicC1rzYKMv9O9OzFS3nKI8DUiH1Ivkfv6jIg7z4gDWKjN92gRWKJZ5uDob4r4aPE36uwaTmnqwShHQ4bo8UR5yI0T0j6fOmtnQH_ZSCTV-v9rH0tdyCYmOfgjrfXia8-YG-APrPsYTz30kB1Od4Rw'
