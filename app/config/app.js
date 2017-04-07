24f99c61d458bc16d580d5e8a884a86183623393
mucktar <git@github.com:mucktarVIP/fcm_manager.git>
2017-04-07 13:35:20 +0700

    update environtment setting

--- app/config/app.js
+++ app/config/app.js
@@ -5,19 +5,19 @@ Confidence = require('confidence');
 store = new Confidence.Store({
   server: {
     $filter: 'env',
-    dev: {
+    local: {
       host: '127.0.0.1',
       port: 8000
     },
-    production: {},
-    $default: {
+    dev: {
       host: '0.0.0.0',
       port: 80
-    }
+    },
+    production: {}
   },
   redis: {
     $filter: 'env',
-    dev: {
+    local: {
       connection: {
         host: "127.0.0.1",
         port: 6379
@@ -30,37 +30,29 @@ store = new Confidence.Store({
         shipment: 5
       }
     },
-    production: {},
-    $default: {
+    dev: {
       connection: {
         host: 'vip-web-session-stg.czrxsu.0001.apse1.cache.amazonaws.com',
         port: 6379
       },
       db: {
         userFcm: 1,
-        cartAbandonment: 0,
+        cartAbandonment: 2,
         bankTransferExpiration: 3,
         pointExpiration: 4,
         shipment: 5
       }
-    }
+    },
+    production: {}
   },
   fcm: {
-    $filter: 'env',
-    production: {},
-    $default: {
-      serverKey: 'AAAA9yvVNzQ:APA91bFplvprQicC1rzYKMv9O9OzFS3nKI8DUiH1Ivkfv6jIg7z4gDWKjN92gRWKJZ5uDob4r4aPE36uwaTmnqwShHQ4bo8UR5yI0T0j6fOmtnQH_ZSCTV-v9rH0tdyCYmOfgjrfXia8-YG-APrPsYTz30kB1Od4Rw'
-    }
+    serverKey: 'AAAA9yvVNzQ:APA91bFplvprQicC1rzYKMv9O9OzFS3nKI8DUiH1Ivkfv6jIg7z4gDWKjN92gRWKJZ5uDob4r4aPE36uwaTmnqwShHQ4bo8UR5yI0T0j6fOmtnQH_ZSCTV-v9rH0tdyCYmOfgjrfXia8-YG-APrPsYTz30kB1Od4Rw'
   },
-  lifetimes: {
-    $filter: 'env',
-    production: {},
-    $default: [
-      1000 * 60 * 60 * 3,
-      1000 * 60 * 60 * 24 * 3,
-      1000 * 60 * 60 * 24 * 7
-    ]
-  }
+  lifetimes: [
+    1000 * 60 * 60 * 3,
+    1000 * 60 * 60 * 24 * 3,
+    1000 * 60 * 60 * 24 * 7
+  ]
 });
 
 criteria = {
