95f58757baee900cb9bbc743e15848f1dc4a95d2
mucktar <git@github.com:mucktarVIP/fcm_manager.git>
2017-04-17 13:35:25 +0700

    change dev configuration for notification lifetimes

--- app/config/app.js
+++ app/config/app.js
@@ -63,11 +63,19 @@ store = new Confidence.Store({
   fcm: {
     serverKey: 'AAAA9yvVNzQ:APA91bFplvprQicC1rzYKMv9O9OzFS3nKI8DUiH1Ivkfv6jIg7z4gDWKjN92gRWKJZ5uDob4r4aPE36uwaTmnqwShHQ4bo8UR5yI0T0j6fOmtnQH_ZSCTV-v9rH0tdyCYmOfgjrfXia8-YG-APrPsYTz30kB1Od4Rw'
   },
-  lifetimes: [
-    1000 * 60 * 60 * 3,
-    1000 * 60 * 60 * 24 * 3,
-    1000 * 60 * 60 * 24 * 7
-  ]
+  lifetimes: {
+    $filter: 'env',
+    dev: [
+      1000 * 60 * 10,
+      1000 * 60 * 20,
+      1000 * 60 * 30
+    ],
+    $default: [
+      1000 * 60 * 60 * 3,
+      1000 * 60 * 60 * 24 * 3,
+      1000 * 60 * 60 * 24 * 7
+    ]
+  }
 });
 
 criteria = {
