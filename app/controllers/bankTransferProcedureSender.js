5dac6cf6c93a0921a4fd52cf53a46a385a91522c
mucktar <git@github.com:mucktarVIP/fcm_manager.git>
2017-05-30 18:01:48 +0700

    banktransfer controller use async/await

--- app/controllers/bankTransferProcedureSender.js
+++ app/controllers/bankTransferProcedureSender.js
@@ -5,10 +5,9 @@ var messageService = require(appRoot + '/app/services/message.js');
 var _ = require('lodash');
 
 var db = config.get('redis').db.bankTransferExpiration;
-var bankTransferConfig = config.get('bankTransfer');
-var sentMessages = [];
 
 function filterMessages(messages){
+  var bankTransferConfig = config.get('bankTransfer');
   var currentTime = new Date().getTime();
   
   var messages = _.filter(messages, function(message){
@@ -23,48 +22,66 @@ function filterMessages(messages){
 }
 
 function processMessages(messages){
-  var sendFcmRequest = _.omitBy(_.map(messages, function(fcmRequest){
-    if(!fcmRequest.error){
-      return fcmRequest.send(config.get('fcm').serverKey)
-    }
-  }), _.isNil);
+  var sendFcmRequest = _.map(messages, function(fcmRequest){
+    return fcmRequest.send(config.get('fcm').serverKey)
+  })
 
-  if (!Object.keys(sendFcmRequest).length) {
-    return Promise.reject('Cannot send message, no fcm_user match');
-  } else {
-    return Promise.all(_.map(sendFcmRequest, function(promise){
-      return promise.catch(function(err){
-        return {error:err};
-      })
-    }));
-  }
+  return Promise.all(_.map(sendFcmRequest, function(promise){
+    return promise.catch(function(err){
+      return {error:err};
+    })
+  }));
 }
 
-module.exports.send = function(request, reply){
-  messageService.fetchQueue(db)
-    .then(function(list){
-      sentMessages = filterMessages(list);
-      if (!sentMessages.length) {
-        return Promise.reject('No message to send');
-      }
-      return messageService.formatMessages(sentMessages, 'bankTransferProcedure');
-    })
-    .then(processMessages)
-    .then(function(){
-      var updateMessage = _.each(sentMessages, function(message){
-        var key = message.user_id + '__' + message.payment_id;
-        var newMessage = message;
-        newMessage.sent_count = message.sent_count + 1;
-        
-        return messageService.updateMessage(db, key, message);
-      });
-      
-      return Promise.all(updateMessage);
-    })
-    .then(function(){
-      reply({message: 'Sent'}).code(200);
-    })
-    .catch(function(err){
-      console.log(err);
+function updateMessage(messages){
+  var updateMessage = _.each(messages, function(message){
+    var key = message.user_id + '__' + message.payment_id;
+    var newMessage = message;
+    newMessage.sent_count = message.sent_count + 1;
+    
+    return messageService.updateMessage(db, key, message);
+  });
+  
+  return Promise.all(updateMessage);
+}
+
+module.exports.send = async function(request, reply){
+  // fetch message list from redis
+  const messages = await messageService.fetchQueue(db)
+    // handle rejection
+    .catch((err) => {
+      reply(Boom.create(400, 'Message list empty'));
     });
+    
+  // if message not empty
+  if (messages && messages.length) {
+    // filter messages which is eligible to send
+    const sentMessages =  filterMessages(messages);
+
+    if (!sentMessages.length) {
+      return reply(Boom.create(400, 'No message to send'));
+    }
+    
+    // format message to fcm request
+    const formattedMessages = await messageService.formatMessages(sentMessages, 'bankTransferProcedure');
+    
+    // filter succesly created message
+    const fcmRequest = _.filter(formattedMessages, (item) => { return !item.error; } );
+    
+    // check if message is succesfully formatted
+    if (!fcmRequest.length) {
+      return reply(Boom.create(400, 'No user match'));
+    } 
+    // send messages
+    await processMessages(fcmRequest);
+    
+    // update sent count
+    return updateMessage(sentMessages)
+      .then((res) => {
+        return reply(Boom.create(200, 'Sent'));
+      })
+      .catch((err) => {
+        return reply(Boom.badRequest(err));
+      });
+  }
 };
